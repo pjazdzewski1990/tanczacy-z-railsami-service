f5255021ab9cd50c72c2b985b46b541ec00e3b30
pjazdzewski1990 <pjazdzewski1990@gmail.com>
2011-12-04 22:21:43 +0100

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TanczacyZRailsamiService::Application.config.secret_token = 'e68b3c30efc0c11e13aa7db7b2a807e98fb4dc1eea6e4d7b20f1aaf65cc7e6c885def29ecd0129a609d1f079f000bd75c0644a28bf95130a9094792fd78460cb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TanczacyZRailsamiService::Application.config.session_store :cookie_store, key: '_tanczacy-z-railsami-service_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TanczacyZRailsamiService::Application.config.session_store :active_record_store
