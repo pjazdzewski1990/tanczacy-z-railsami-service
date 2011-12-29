075d61cb844ad64382bbcb7650331d93f8762b75
Mateusz Szygenda <mateusz.szygenda@gmail.com>
2011-12-29 17:10:28 +0100

    implemented facebook statuses fetching

---
+++ config/initializers/collectors.rb
@@ -0,0 +1,6 @@
+COLLECTORS_SETTINGS = {
+  facebook: {
+    client_id: 'ENTER_CLIENT_ID',
+    secret: 'ENTER_SECRET'
+  }
+}
--- lib/collectors/abstract_collector.rb
+++ lib/collectors/abstract_collector.rb
@@ -3,7 +3,7 @@ class AbstractCollector
   # It's purpose is to provide reference for required methods in collectors
 
   # This method should retrieve data and store it in DB
-  def collect(collector_context)
+  def collect(accounts)
     raise "Collect not implemented in #{self.class.name}"
   end 
 
@@ -14,11 +14,28 @@ class AbstractCollector
 
   # It also provides some helper methods for all collectors
 
+  attr_accessor :settings
   # It allows to store some context data into database which 
   # will allow to continue work in exact same place during next "collect" 
   #
   # i.e. It could store uid of latest account retrieved
   def store_context(context)
 
+  end
+
+  def initialize(settings)
+    @settings = settings
+  end
+  
+  def save_resources(resources)
+    resources.each do |resource|
+      puts resource
+
+      Rails.logger.info(
+      'Resource: ' +
+      resource[:created_at] +
+      ' ' +
+      resource[:content])
+    end
   end
 end
