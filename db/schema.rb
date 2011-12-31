5ae07e8378913b99a1d149c67e1b28ceb7f30f0d
pjazdzewski1990 <pjazdzewski1990@gmail.com>
2011-12-31 12:14:33 +0100

    rozwiniete UserProxy

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111210093124) do
+ActiveRecord::Schema.define(:version => 20111231094904) do
 
   create_table "resources", :force => true do |t|
     t.string   "title"
@@ -37,8 +37,15 @@ ActiveRecord::Schema.define(:version => 20111210093124) do
   end
 
   create_table "user_proxies", :force => true do |t|
+    t.integer  "service_id"
+    t.string   "uid"
+    t.string   "token"
+    t.string   "refresh"
+    t.date     "date"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "user_proxies", ["service_id"], :name => "index_user_proxies_on_service_id"
 
 end
